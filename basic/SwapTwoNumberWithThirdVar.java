63fd5988cafd6d49da856d655a16a23f56f882f7
TankT40 <vikaspawar9130@gmail.com>
2023-02-22 23:08:29 +0530

    22-02-2023

--- basic/SwapTwoNumberWithThirdVar.java
+++ basic/SwapTwoNumberWithThirdVar.java
@@ -1,5 +1,19 @@
 package basic;
 
 public class SwapTwoNumberWithThirdVar {
-    
+    public static void swapTwoNumberWithThirdVar(int a, int b){
+        System.out.println("Before swapping a : "+a);
+        System.out.println("Before swapping b : "+b);
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping a : "+a);
+        System.out.println("After swapping b : "+b);
+
+    }
+    public static void main(String[] args) {
+        int a =10, b=20;
+        swapTwoNumberWithThirdVar(a, b);
+        
+    }
 }
