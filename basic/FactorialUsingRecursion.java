2a26d2b3c38a720cd42d5e657c48c03032ceaa90
TankT40 <vikaspawar9130@gmail.com>
2023-02-25 22:36:15 +0530

    25-02-2023

--- basic/FactorialUsingRecursion.java
+++ basic/FactorialUsingRecursion.java
@@ -1,5 +1,16 @@
 package basic;
 
 public class FactorialUsingRecursion {
-    
+    public static int factorialUsingRecursion(int num){
+     
+        if(num==0)
+            return 1;
+        else
+            return num * factorialUsingRecursion(num-1);
+        
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(factorialUsingRecursion(num));
+    }
 }
