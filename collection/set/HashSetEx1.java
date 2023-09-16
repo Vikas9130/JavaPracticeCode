f19ac1b3c65750239cfc28022246a49593cabfe6
Vikas9130 <vikaspawar9130@gmail.com>
2023-09-16 12:10:11 +0530

    learning

--- collection/set/HashSetEx1.java
+++ collection/set/HashSetEx1.java
@@ -1,6 +1,12 @@
 package collection.set;
 
+import java.util.HashSet;
+
 public class HashSetEx1 {
-    
+    public static void main(String[] args) {
+      HashSet<String> set = new HashSet<>();
+      
+        
+    }
     
 }
